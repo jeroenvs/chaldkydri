3f5a59f69b447793b168241de149289f71f44690
Jeroen van Schagen <jeroen@it-techniek.nl>
2009-11-16 13:13:00 +0100

    Added post form partial and made posts order desc on creation_date, showing the newest post at the top. Also includded comment information beneat the post partial.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.xml
   def index
-    @posts = Post.all
+    @posts = Post.find(:all, :order => "created_at DESC")
 
     respond_to do |format|
       format.html # index.html.erb
